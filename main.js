c6db6cbc92006a829e12d88bb4adfc6d0c635a30
dabah aissa <qyosk727@gmail.com>
2024-09-29 10:48:34 +0200

    none

---
+++ main.js
@@ -0,0 +1,59 @@
+function abarea(a, b) {
+  let area = a * b;
+  return area;
+}
+let c = abarea(5, 5);
+console.log(c);
+
+/*function abaissa (a , b) { 
+    let aissa = a +b ;
+     return aissa;
+ 
+ }
+ let b =abaissa(6 ,6)
+ console.log(b)
+ 
+ 
+ 
+ function abhamid (a ,b) {
+     let hamid = a- b ;
+     return hamid
+ }
+ let a =abhamid(10 ,10)
+ console.log(a
+ 
+ )*/
+/*function data (a ,b ,c ) {
+    let moi = a+ b+ c ;
+    return moi;
+}    
+let na = data("youer name" + " aissa", " dabah" , " alg")
+console.log(na);*/
+
+
+
+/*function mat(a, b) {
+    let n = a * b;
+    return n;
+  }
+  
+  let f = mat( 50 , 20);
+  console.log("area "+ f );
+
+  function x (a, b ,c) {
+    let sv  =(a + b)  * c;
+    return sv;
+  }
+  
+  let tst = x ( 50 , 20 ,2);
+  console.log("area "+ tst );*/
+
+
+
+
+
+    console.log(Math.round(0.9));
+// Expected output: 1
+
+console.log(Math.round(5.95), Math.round(5.5), Math.round(5.05));
+// Expected output: 6 6   n nn      nbbbbbbbbbbbb                                                                                                                                        b                                                                                                                                                                                                                                                                                                                         bb vb bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb 
